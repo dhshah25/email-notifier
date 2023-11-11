e94a7df9a64f087788e4578812b15755548c57c3
dhshah25 <141484056+dhshah25@users.noreply.github.com>
2023-11-11 19:02:03 +0530

    Update settings.py

--- networkhub/settings.py
+++ networkhub/settings.py
@@ -147,4 +147,4 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER='dhairyacars2003@gmail.com'
-EMAIL_HOST_PASSWORD='eliccawpoimrmtpk'
+EMAIL_HOST_PASSWORD='{your password}'
